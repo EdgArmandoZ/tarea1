13e6d43433a58846a1af3818a96d593878728e5b
Edgarwebb <161100457+Edgarwebb@users.noreply.github.com>
2024-07-08 15:46:44 -0600

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Component , Inject} from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tarea1';
+  resultado='';
+
+  formularioContacto= new FormGroup({
+    nombre: new FormControl('',[Validators.required, Validators.minLength(10)]),
+    edad: new FormControl('',[Validators.required, Validators.minLength(1)]),
+    DPI: new FormControl('',[Validators.required, Validators.minLength(13)]),
+  })
+//Local Storage
+
+listaPersonas:{nombre:string, edad:string, DPI:string}[]=[];
+constructor(@Inject(DOCUMENT) private document:Document){
+  const localStorage= document.defaultView?.localStorage;
+
+  let datos = localStorage?.getItem("Personas");
+  if(datos != null){
+    let arreglo = JSON.parse(datos);
+    if(arreglo != null){
+      this.listaPersonas= arreglo;
+    }
+  }
+}
+
+agregarPersona(){
+  if(this.formularioContacto.value){
+    const nuevaPersona={
+      nombre: this.formularioContacto.value.nombre!,
+      edad: this.formularioContacto.value.edad!,
+      DPI: this.formularioContacto.value.DPI!,
+    };
+    this.listaPersonas.push(nuevaPersona);
+    this.actualizarLocalStorage();
+    this.formularioContacto.reset();
+    this.resultado="todos los datos validos"
+  } else{
+    this.resultado= "Hay datos invalidos"
+  }
+}
+
+borrarPersona1(index: number){
+  this.listaPersonas.splice(index, 1);
+  this.actualizarLocalStorage();
+}
+
+actualizarLocalStorage(){
+  localStorage.setItem("personas", JSON.stringify(this.listaPersonas));
+}
 }
